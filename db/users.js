e38980007235ee16679b25e9b12bbdc7eeacddb7
Jonathon Kresner <jkresner@gmail.com>
2016-06-30 20:18:03 +1000

    Basic starer query

---
+++ db/users.js
@@ -0,0 +1,413 @@
+[
+    {
+    "_id" : ObjectId("55e47af243868c1100927bba"),
+    "cohort" : {
+        "engagement" : {
+            "visit_first" : ISODate("2015-08-31T16:03:37.548Z"),
+            "visit_signup" : ISODate("2015-08-31T16:04:02.482Z")
+        },
+        "firstRequest" : {
+            "url" : "/"
+        },
+        "aliases" : [ 
+            "BtZEeq8mzUvmIb4orYRZU5-ytaw_HX_H", 
+            "HnyRllwVBMki80mDbjn8n5p0dkM5xgss", 
+            "AGCUlipONTsmOBe832pzrZveWfoJaG-w"
+        ]
+    },
+    "emailVerified" : true,
+    "name" : "Prateek Gupta",
+    "email" : "prateek@prateekgupta.org",
+    "location" : {
+        "timeZoneId" : "Australia/Sydney",
+        "name" : "Sydney NSW, Australia",
+        "shortName" : "Sydney"
+    },
+    "auth" : {
+        "gp" : {
+            "email" : "prateek@prateekgupta.org",
+            "domain" : "prateekgupta.org",
+            "verified" : false,
+            "circledByCount" : 64,
+            "language" : "en",
+            "isPlusUser" : true,
+            "placesLived" : [ 
+                {
+                    "primary" : true,
+                    "value" : "San Jose, CA"
+                }
+            ],
+            "organizations" : [ 
+                {
+                    "primary" : false,
+                    "endDate" : "2010",
+                    "type" : "school",
+                    "name" : "San Jose State University"
+                }
+            ],
+            "image" : {
+                "isDefault" : false,
+                "url" : "https://lh6.googleusercontent.com/-DlcrxSMtLzY/AAAAAAAAAAI/AAAAAAAAABE/ZDcOs6n8Y5Q/photo.jpg?sz=50"
+            },
+            "url" : "https://plus.google.com/+PrateekGuptaSF",
+            "name" : {
+                "givenName" : "Prateek",
+                "familyName" : "Gupta"
+            },
+            "displayName" : "Prateek Gupta",
+            "id" : "113692111376615944506",
+            "objectType" : "person",
+            "emails" : [ 
+                {
+                    "type" : "account",
+                    "value" : "prateek@prateekgupta.org"
+                }
+            ],
+            "gender" : "male",
+            "etag" : "\"i9aZP8TD8jXVPIxD0T0PWsMRx6s/x5ehcRV0gvKh1gcEUpo6S9PrMnQ\"",
+            "kind" : "plus#person"
+        }
+    }
+}
+/* 0 */
+{
+    "_id" : ObjectId("5367fd6d1c67d1a4859d30e7"),
+    "cohort" : {
+        "engagement" : {
+            "visit_first" : ISODate("2014-05-05T21:06:53.000Z"),
+            "visit_signup" : ISODate("2014-05-05T21:06:53.000Z")
+        },
+        "aliases" : [ 
+            "uTmytRmXXUsv6KNr3KyFafkOX_5N4rDW", 
+            "03N0vX4MMTNiwyzz3M38nDdsutJTHj6q", 
+            "7fYVykIEWG5fKg6VWujCyLa6TbMxKK23", 
+            "w9hZ09rPpd9Zz7Zthwoxdg7GA4bj-0ar", 
+            "W9FFR880LTvdu8E7x5PqDA9BgVqQ1Gpf", 
+            "OQ1SMJwkWLGGXe9wb7kB80LYo8QUNxED", 
+            "CGhSG9in1tEhnpj4solCs1roi2a--5Vs", 
+            "USYpPWNKtfZlHmhmDrEbGK_lOHe03sE-"
+        ],
+        "firstRequest" : {
+            "url" : "/"
+        }
+    },
+    "email" : "pg@airpair.com",
+    "initials" : "PG",
+    "name" : "Prateek Gupta",
+    "username" : "prtkgpt",
+    "bio" : "AirPair is about developers helping developers. We’re an early-stage YCombinator startup on a mission to transfer software knowledge around the world using the power of video chat & pair programming. We plan to build AirPair.com into one of the web’s top 10 software learning resources.",
+    "primaryPayMethodId" : ObjectId("553c0a3bd4d5b111008a6dd1"),
+    "location" : {
+        "timeZoneId" : "America/Los_Angeles",
+        "name" : "San Francisco, CA, USA",
+        "shortName" : "San Francisco"
+    },
+    "auth" : {
+        "gp" : {
+            "email" : "pg@airpair.com",
+            "domain" : "airpair.com",
+            "verified" : false,
+            "circledByCount" : 31,
+            "language" : "en",
+            "isPlusUser" : true,
+            "placesLived" : [ 
+                {
+                    "primary" : true,
+                    "value" : "San Francisco, CA, United States"
+                }
+            ],
+            "image" : {
+                "isDefault" : false,
+                "url" : "https://lh3.googleusercontent.com/-Bhv-iDDFOEs/AAAAAAAAAAI/AAAAAAAAABE/bOpMFrgZmQw/photo.jpg?sz=50"
+            },
+            "url" : "https://plus.google.com/+PrateekGuptaAP",
+            "aboutMe" : "I am based in San Francisco, and deeply involved with startups. I am an enthusiast for hackathons, and I love innovation. Great stories are always preferred.",
+            "braggingRights" : "I conduct StartupBus from San Francisco.",
+            "tagline" : "I am Prateek Gupta, with AirPair.com",
+            "name" : {
+                "givenName" : "Prateek",
+                "familyName" : "Gupta"
+            },
+            "displayName" : "Prateek Gupta",
+            "id" : "110496794584456738170",
+            "objectType" : "person",
+            "emails" : [ 
+                {
+                    "type" : "account",
+                    "value" : "pg@airpair.com"
+                }
+            ],
+            "occupation" : "Head of Operations",
+            "etag" : "\"RqKWnRU4WW46-6W3rWhLR9iFZQM/Jfa4jbrAxGtU3rXifVF4MDMTfHs\"",
+            "kind" : "plus#person",
+            "tokens" : {}
+        },
+        "gh" : {
+            "login" : "prtkgpt",
+            "id" : 2454349,
+            "avatar_url" : "https://avatars.githubusercontent.com/u/2454349?v=3",
+            "gravatar_id" : "",
+            "name" : "Prateek Gupta",
+            "company" : "Airpair Inc",
+            "blog" : "http://www.prateekgupta.org",
+            "location" : "United States",
+            "email" : "pg@airpair.com",
+            "hireable" : false,
+            "bio" : null,
+            "public_repos" : 12,
+            "public_gists" : 1,
+            "followers" : 2,
+            "following" : 10,
+            "created_at" : "2012-09-29T21:22:00Z",
+            "updated_at" : "2015-03-27T21:14:29Z",
+            "tokens" : {}
+        },
+        "in" : {
+            "lastName" : "Gupta",
+            "id" : "FtpSNLaCA6",
+            "firstName" : "Prateek",
+            "tokens" : { }
+        },
+        "tw" : {
+            "needs_phone_verification" : false,
+            "suspended" : false,
+            "notifications" : false,
+            "following" : false,
+            "profile_banner_url" : "https://pbs.twimg.com/profile_banners/42587892/1393469941",
+            "profile_image_url" : "http://pbs.twimg.com/profile_images/461627421720969216/M1aBVSeN_normal.jpeg",
+            "lang" : "en",
+            "verified" : false,
+            "geo_enabled" : true,
+            "time_zone" : "Pacific Time (US & Canada)",
+            "utc_offset" : -28800,
+            "favourites_count" : 2482,
+            "created_at" : "Tue May 26 08:15:53 +0000 2009",
+            "listed_count" : 42,
+            "friends_count" : 632,
+            "followers_count" : 517,
+            "protected" : false,
+            "url" : "http://t.co/C50Yhzy9Jy",
+            "description" : "Head of Operations @airpair, SF Conductor @TheStartupBus, @DEMO Alumni, entrepreneur, traveler, foodie, and the family guy!",
+            "profile_location" : null,
+            "location" : "San Francisco Bay Area",
+            "screen_name" : "prtkgpt",
+            "name" : "Prateek Gupta",
+            "id" : 42587892,
+            "tokens" : {}
+        },
+        "al" : {
+            "scopes" : [ 
+                "email", 
+                "talent"
+            ],
+            "investor" : true,
+            "skills" : [ 
+                {
+                    "level" : 4,
+                    "angellist_url" : "https://angel.co/team-building",
+                    "display_name" : "Team Building",
+                    "name" : "team building",
+                    "tag_type" : "SkillTag",
+                    "id" : 15708
+                }, 
+                {
+                    "level" : 3,
+                    "angellist_url" : "https://angel.co/leadership-1",
+                    "display_name" : "Leadership",
+                    "name" : "leadership",
+                    "tag_type" : "SkillTag",
+                    "id" : 18318
+                }, 
+                {
+                    "level" : 3,
+                    "angellist_url" : "https://angel.co/product-management",
+                    "display_name" : "Product Management",
+                    "name" : "product management",
+                    "tag_type" : "SkillTag",
+                    "id" : 15872
+                }, 
+                {
+                    "level" : 3,
+                    "angellist_url" : "https://angel.co/social-media-marketing-1",
+                    "display_name" : "Social Media Marketing",
+                    "name" : "social media marketing",
+                    "tag_type" : "SkillTag",
+                    "id" : 17587
+                }, 
+                {
+                    "level" : 3,
+                    "angellist_url" : "https://angel.co/entrepreneurship",
+                    "display_name" : "Entrepreneurship",
+                    "name" : "entrepreneurship",
+                    "tag_type" : "SkillTag",
+                    "id" : 15745
+                }, 
+                {
+                    "level" : 3,
+                    "angellist_url" : "https://angel.co/product-development",
+                    "display_name" : "Product Development",
+                    "name" : "product development",
+                    "tag_type" : "SkillTag",
+                    "id" : 18317
+                }, 
+                {
+                    "level" : 3,
+                    "angellist_url" : "https://angel.co/business-strategy",
+                    "display_name" : "Business Strategy",
+                    "name" : "business strategy",
+                    "tag_type" : "SkillTag",
+                    "id" : 16083
+                }, 
+                {
+                    "level" : 2,
+                    "angellist_url" : "https://angel.co/venture-fundraising",
+                    "display_name" : "Venture Fundraising",
+                    "name" : "venture fundraising",
+                    "tag_type" : "SkillTag",
+                    "id" : 14771
+                }, 
+                {
+                    "level" : 2,
+                    "angellist_url" : "https://angel.co/venture-fundraising",
+                    "display_name" : "Venture Fundraising",
+                    "name" : "venture fundraising",
+                    "tag_type" : "SkillTag",
+                    "id" : 14771
+                }
+            ],
+            "roles" : [ 
+                {
+                    "angellist_url" : "https://angel.co/marketing-2",
+                    "display_name" : "Marketing",
+                    "name" : "marketing",
+                    "tag_type" : "RoleTag",
+                    "id" : 80489
+                }, 
+                {
+                    "angellist_url" : "https://angel.co/sales-2",
+                    "display_name" : "Sales",
+                    "name" : "sales",
+                    "tag_type" : "RoleTag",
+                    "id" : 80488
+                }, 
+                {
+                    "angellist_url" : "https://angel.co/angels",
+                    "display_name" : "Angel",
+                    "name" : "angels",
+                    "tag_type" : "RoleTag",
+                    "id" : 9300
+                }, 
+                {
+                    "angellist_url" : "https://angel.co/seed-funds",
+                    "display_name" : "Seed Fund",
+                    "name" : "seed funds",
+                    "tag_type" : "RoleTag",
+                    "id" : 9305
+                }, 
+                {
+                    "angellist_url" : "https://angel.co/office_manager",
+                    "display_name" : "Office Manager",
+                    "name" : "office_manager",
+                    "tag_type" : "RoleTag",
+                    "id" : 103480
+                }, 
+                {
+                    "angellist_url" : "https://angel.co/mentor-6",
+                    "display_name" : "Mentor",
+                    "name" : "Mentor",
+                    "tag_type" : "RoleTag",
+                    "id" : 151070
+                }, 
+                {
+                    "angellist_url" : "https://angel.co/operations-1",
+                    "display_name" : "Operations",
+                    "name" : "operations",
+                    "tag_type" : "RoleTag",
+                    "id" : 129175
+                }
+            ],
+            "locations" : [ 
+                {
+                    "angellist_url" : "https://angel.co/san-francisco",
+                    "display_name" : "San Francisco",
+                    "name" : "san francisco",
+                    "tag_type" : "LocationTag",
+                    "id" : 1692
+                }
+            ],
+            "criteria" : "",
+            "what_i_do" : null,
+            "what_ive_built" : "I co-founded my first company at a hackathon in June 2012, and launched it at Demo.com Fall Conference in October 2012 with a full scholarship and valuation of $1.5 million. Generating over five thousand users in first 3 months with zero marketing budget.",
+            "resume_url" : "",
+            "behance_url" : "",
+            "dribbble_url" : "",
+            "github_url" : "",
+            "aboutme_url" : "",
+            "linkedin_url" : "http://www.linkedin.com/in/prtkgpt",
+            "facebook_url" : "http://www.facebook.com/prtkgpt",
+            "twitter_url" : "http://twitter.com/#!/prtkgpt",
+            "online_bio_url" : "http://www.prateekgupta.org",
+            "blog_url" : "",
+            "email" : "prateek@prateekgupta.org",
+            "image" : "https://d1qb2nb5cznatu.cloudfront.net/users/176172-medium_jpg?1405507881",
+            "angellist_url" : "https://angel.co/prtkgpt",
+            "follower_count" : 274,
+            "bio" : "Head of Operations @airpair, 1st @mit Entrepreneurship Bootcamp, SF Conductor @TheStartupBus, @demo Alumni, entrepreneur, foodie, and the family guy! ",
+            "id" : 176172,
+            "name" : "Prateek Gupta",
+            "tokens" : { }
+        }
+    },
+    "legacy" : {
+        "tags" : [ 
+            {
+                "sort" : 0,
+                "tagId" : ObjectId("5149dccb5fc6390200000013"),
+                "_id" : ObjectId("5457addb83272a0b00e6fb9d")
+            }, 
+            {
+                "sort" : 0,
+                "tagId" : ObjectId("514825fa2a26ea0200000028"),
+                "_id" : ObjectId("5457addf83272a0b00e6fb9e")
+            }, 
+            {
+                "sort" : 0,
+                "tagId" : ObjectId("52ffea1a1c67d1a4859d2333"),
+                "_id" : ObjectId("5457ade483272a0b00e6fba0")
+            }, 
+            {
+                "sort" : 0,
+                "tagId" : ObjectId("5181d0aa66a6f999a465eceb"),
+                "_id" : ObjectId("54904a27fcacde0b009f30ea")
+            }, 
+            {
+                "sort" : 0,
+                "tagId" : ObjectId("5556470b0dd191110059c2a1"),
+                "_id" : ObjectId("5556472a0dd191110059c2a5")
+            }
+        ],
+        "bookmarks" : [ 
+            {
+                "_id" : ObjectId("545a5b14a954070b00e7cfa7"),
+                "objectId" : ObjectId("544a3f30339c940b0079445a"),
+                "type" : "post",
+                "sort" : 0
+            }, 
+            {
+                "_id" : ObjectId("545a5b2aa954070b00e7cfac"),
+                "objectId" : ObjectId("5459bee2c8df0c0b00df6258"),
+                "type" : "post",
+                "sort" : 2
+            }, 
+            {
+                "_id" : ObjectId("545a5b49c03b5a0b007265d4"),
+                "objectId" : ObjectId("542c4b4f8e66ce0b00c885a4"),
+                "type" : "post",
+                "sort" : 1
+            }
+        ]
+    }
+}
+
+
+]
